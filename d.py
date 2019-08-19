721625b90db0bada0b463c8a50c0166f0b678942
Phantom2018 <805410919@qq.com>
2019-08-19 21:50:24 +0800

    commit file d.py

--- d.py
+++ d.py
@@ -1,6 +1,7 @@
+
 import numpy as np
 import operator
-from
+from os import listdir
 
 def img2vector(filename):
     returnVect = np.zeros((1, 1024))
@@ -28,4 +29,51 @@ def classify0(inX, dataSet, labels, k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
 
+    sortedClassCount = sorted(
+            classCount.items(), key=operator.itemgetter(1), reverse=True)
 
+    return sortedClassCount[0][0]
+
+
+def handwritingClassTest():
+    # category lable list of yangbendata
+    hwLabels = []
+
+    # yangbendata file list
+    trainingFileList = listdir('digits/trainingDigits')
+    m = len(trainingFileList)
+    # init yangbendata matrix
+    trainingMat = np.zeros((m, 1024))
+    # read all yangbendata to data-matrix
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr) # the number that training file refers to
+        # save yangbendata into matrix
+        trainingMat[i, :] = img2vector('digits/trainingDigits/%s' % fileNameStr)
+
+
+    testFileList = listdir('digits/testDigits')
+    errorCount = 0.0
+    # loop-test each testFile
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        classNumStr = int(fileStr.split('_')[0])
+
+        # get data-matrix
+        vectorUnderTest = img2vector('digits/trainingDigits/%s' % fileNameStr)
+        # classify
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("test sample: %d, classifier predict: %d, fact label: %d " %
+                (i+1, classifierResult, classNumStr))
+
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+
+        print("\nerror rate: %f" % (errorCount/float(mTest)))
+
+handwritingClassTest()
